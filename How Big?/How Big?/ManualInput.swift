2f841d085d16d5e6e5048449435504e98f60be5d
ChamodKalupahana <Chamodkalupahana@gmail.com>
2025-02-18 20:49:25 +0000

    started adding manual input file

---
+++ How Big?/How Big?/ManualInput.swift
@@ -0,0 +1,18 @@
+//
+//  ManualInput.swift
+//  How Big?
+//
+//  Created by User on 18/02/2025.
+//
+
+import SwiftUI
+
+struct ManualInput: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    ManualInput()
+}
