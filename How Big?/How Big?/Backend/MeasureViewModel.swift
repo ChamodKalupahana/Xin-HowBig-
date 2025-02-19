1735fc5a30cde8f387aa309d8bebe5e4d18e7efb
ChamodKalupahana <Chamodkalupahana@gmail.com>
2025-02-19 23:08:19 +0000

    started adding measureviewmodel

---
+++ How Big?/How Big?/Backend/MeasureViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  MeasureViewModel.swift
+//  How Big?
+//
+//  Created by User on 19/02/2025.
+//
+
+import Foundation
+import SwiftUI
+
+class MeasureViewModel : ObservableObject {
+    
+    @Published var isShowingReferenceScreen : Bool = false
+    @Published var referenceToMeasure : ReferenceObject = ReferenceObject(name: "New Reference", length: 0, image: Image(systemName : "testtube.2"), numberOfDimensions: 1, source: .personal)
+}
