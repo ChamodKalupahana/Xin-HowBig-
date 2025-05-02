5690645661c5549cde9b0ff4d19d7bebe6ffde52
ChamodKalupahana <Chamodkalupahana@gmail.com>
2025-05-03 00:11:29 +0100

    defining rotation

--- How Big?/How Big?/Backend/SceneViewController.swift
+++ How Big?/How Big?/Backend/SceneViewController.swift
@@ -13,12 +13,27 @@ struct SceneViewController : UIViewRepresentable {
     func makeUIView(context: Context) -> SCNView {
         let sceneView = SCNView()
         
+        // Create Scene
         let scene = SCNScene()
         sceneView.scene = scene
         sceneView.autoenablesDefaultLighting = true
+        
+        // Load Object
+        if let objectNode = SCNScene(named: "art.scnassets/Cottage_FREE")?.rootNode.clone() {
+            scene.rootNode.addChildNode(objectNode)
+            // rotate
+            
+        }
     }
     
     func updateUIView(_ uiView: SCNView, context: Context) {
+    }
+    
+    func rotate(node : SCNNode) {
+        let spin = CABasicAnimation(keyPath: "rotation")
+        spin.fromValue = SCNVector4(0, 1, 0, 0)
+        spin.toValue = SCNVector4(0, 1, 0, Float.pi * 2)
+        spin.duration
     }
 }
 
