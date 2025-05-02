cb2e9bc616003ac59b075577e116c14fbee5e78d
ChamodKalupahana <Chamodkalupahana@gmail.com>
2025-05-03 00:06:12 +0100

    adding scene

--- How Big?/How Big?/Backend/SceneViewController.swift
+++ How Big?/How Big?/Backend/SceneViewController.swift
@@ -11,7 +11,14 @@ import SceneKit
 
 struct SceneViewController : UIViewRepresentable {
     func makeUIView(context: Context) -> SCNView {
+        let sceneView = SCNView()
         
+        let scene = SCNScene()
+        sceneView.scene = scene
+        sceneView.autoenablesDefaultLighting = true
+    }
+    
+    func updateUIView(_ uiView: SCNView, context: Context) {
     }
 }
 
