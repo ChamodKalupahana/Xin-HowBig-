3444d7db2cf71613a40d1cd257a3f6ab7ae11e5c
ChamodKalupahana <Chamodkalupahana@gmail.com>
2025-05-03 01:28:05 +0100

    adding panning

--- How Big?/How Big?/Backend/SceneViewController.swift
+++ How Big?/How Big?/Backend/SceneViewController.swift
@@ -29,6 +29,7 @@ struct SceneViewController : UIViewRepresentable {
         // add XYZ axes yay
         addXYZAxes(to: scene.rootNode)
         
+        // add in panning
         context.coordinator.sceneView = sceneView
         let panGesture = UIPanGestureRecognizer(target: context.coordinator, action: #selector(ObjectInteractionCoordinator.handlePan(_:)))
         sceneView.addGestureRecognizer(panGesture)
