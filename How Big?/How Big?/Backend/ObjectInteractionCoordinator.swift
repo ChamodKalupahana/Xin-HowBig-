c56d4dfa54771d02cbbfc8af12be8a54532fc79d
ChamodKalupahana <Chamodkalupahana@gmail.com>
2025-05-03 01:12:27 +0100

    started add ing new file

--- How Big?/How Big?/Backend/ObjectInteractionCoordinator.swift
+++ How Big?/How Big?/Backend/ObjectInteractionCoordinator.swift
@@ -7,12 +7,6 @@
 
 import SwiftUI
 
-struct ObjectInteractionCoordinator: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-#Preview {
-    ObjectInteractionCoordinator()
+class ObjectInteractionCoordinator : NSObject {
+    
 }
