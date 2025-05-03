73203ca3c7125da5667e6b35abd45c3992202c3c
ChamodKalupahana <Chamodkalupahana@gmail.com>
2025-05-03 01:12:13 +0100

    added name

---
+++ How Big?/How Big?/Backend/ObjectInteractionCoordinator.swift
@@ -0,0 +1,18 @@
+//
+//  ObjectInteractionCoordinator.swift
+//  How Big?
+//
+//  Created by User on 03/05/2025.
+//
+
+import SwiftUI
+
+struct ObjectInteractionCoordinator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ObjectInteractionCoordinator()
+}
