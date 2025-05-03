9029337737aa9dde28ed06eb1bb26e990743b8e2
ChamodKalupahana <Chamodkalupahana@gmail.com>
2025-05-03 01:39:48 +0100

    added y rotation

--- How Big?/How Big?/Backend/ObjectInteractionCoordinator.swift
+++ How Big?/How Big?/Backend/ObjectInteractionCoordinator.swift
@@ -35,6 +35,8 @@ class ObjectInteractionCoordinator : NSObject {
 //            node.position.x += deltaX
 //            node.position.y -= deltaY
             
+            let yRotation = SCNMatrix4MakeRotation(-deltaX, 0, 1, 0)
+            
             gesture.setTranslation(.zero, in: sceneView)
             
         case .ended, .cancelled:
