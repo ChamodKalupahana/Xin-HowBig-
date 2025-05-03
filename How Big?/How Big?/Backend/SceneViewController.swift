d1a8385612ac929538b5091170f31388e1248252
ChamodKalupahana <Chamodkalupahana@gmail.com>
2025-05-03 01:58:22 +0100

    added object back

--- How Big?/How Big?/Backend/SceneViewController.swift
+++ How Big?/How Big?/Backend/SceneViewController.swift
@@ -21,14 +21,14 @@ struct SceneViewController : UIViewRepresentable {
         let containerNode = SCNNode()
         scene.rootNode.addChildNode(containerNode)
         
-        // Load Object
-//        if let objectNode = SCNScene(named: "art.scnassets/Cottage_FREE.dae")?.rootNode.clone() {
-//            containerNode.addChildNode(objectNode)
-//            // for automatic rotation
-////            rotate(node: objectNode)
-//            context.coordinator.currentNode = containerNode
-//            
-//        } //
+//         Load Object
+        if let objectNode = SCNScene(named: "art.scnassets/Cottage_FREE.dae")?.rootNode.clone() {
+            containerNode.addChildNode(objectNode)
+            // for automatic rotation
+//            rotate(node: objectNode)
+            context.coordinator.currentNode = containerNode
+            
+        } //
         
         // add XYZ axes yay
         addXYZAxes(to: containerNode)
